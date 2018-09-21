7156a95de257a925bcfc05d1ccad0363270a7e75
minaamfasihi <minaam.fasihi@gmail.com>
2018-09-21 18:49:14 +0500

    nested statements, scopes

--- nested-statements-and-scopes.py
+++ nested-statements-and-scopes.py
@@ -21,4 +21,37 @@ print(printer())
  # 3) global
  # 4) built-in
 
- 
+def greet():
+    # Enclosing function
+    name = 'Sammy'
+
+    def hello():
+        print('Hello ' + name)
+
+    hello()
+
+greet()
+
+# scope of the variable is the block they are declared in starting from the point of definition of the name
+# when you declare a variable inside a function, they are not related in any way to other variables
+# with the same names used outside the function
+
+x = 50
+def func(x):
+    print('x is ', x) # 50
+    x = 2
+    print('Changed local x to ', x) # 2
+
+func(x)
+print('x is still ', x) # 50
+
+
+x = 50
+def new_func():
+    global x # this means that whenever I use x now, use the global x, not local
+    print(x) # 50
+    x = 2 # 2
+
+print(x) # 50
+new_func()
+print(x) # 2
