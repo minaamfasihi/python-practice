807f5a436a584bce04ad99fc56b06472fc4c2acd
minaamfasihi <minaam.fasihi@gmail.com>
2018-10-07 20:07:43 +0500

    more stuff

---
+++ all-any.py
@@ -0,0 +1,6 @@
+# if all the elements are true, then all returns true
+# any returns true if any of them is true
+
+l = [True, True, False, False]
+print(all(l))
+print(any(l))
---
+++ complex.py
@@ -0,0 +1,3 @@
+print(complex(2, 3)) # 2 is the real part, 3 imaginary
+
+print(complex('10+2j')) # 10 real, 2 imaginary
